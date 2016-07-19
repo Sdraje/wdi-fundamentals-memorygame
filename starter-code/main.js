17817779dabd16854990ec0c5ea72ae896cae861
Alfredo Maria Milano <Alfredo@alfredos-mbp.home>
2016-07-19 15:49:46 +0100

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,9 @@ var cardOne = "queen"
 var cardTwo = "king"
 var cardThree = "queen"
 var cardFour = "king"
+
+if (cardOne == cardTwo) {
+	alert('You found a match!')
+} else {
+	alert('Sorry, try again!')
+	}
